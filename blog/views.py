d4fca8a3646022cfffbb079723195c560da480ce
kerem212012 <rockstarfoxykerem@gmail.com>
2025-01-18 11:53:37 +0300

    add template logic to post page

--- blog/views.py
+++ blog/views.py
@@ -55,6 +55,7 @@ def post_detail(request, slug):
         "text": post.text,
         "author": post.author.username,
         "comments": serialized_comments,
+        "comments_count": len(comments),
         'likes_amount': post.likes.count(),
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
